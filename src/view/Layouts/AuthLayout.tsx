97e10da3420a35c0ce1f02cf223a8e56c412d340
rafaapcode <rafaap2003@gmail.com>
2025-06-18 12:07:54 -0300

    feat: creating the first part of the login page

--- src/view/Layouts/AuthLayout.tsx
+++ src/view/Layouts/AuthLayout.tsx
@@ -1,13 +1,26 @@
-import { Outlet } from "react-router-dom"
+import illustration from "../../assets/illustration.png";
+import Logo from "../components/Logo";
 
 function AuthLayout() {
   return (
-    <div>
-      <h1>AuthLayout</h1>
+    <div className="flex w-full h-full">
+      <div className="w-1/2 h-full"></div>
+      <div className="w-1/2 h-full flex justify-center relative items-center p-8">
+        <img
+          src={illustration}
+          alt="Image of login page"
+          className="object-cover w-full h-full max-w-[656px] max-h-[960px] select-none rounded-4xl"
+        />
 
-      <Outlet />
+        <div className="max-w-[656px] bottom-8 bg-white h-auto p-10 absolute rounded-b-4xl">
+          <Logo className="text-teal-700 h-8"/>
+          <p  className="text-gray-700 font-medium text-xl mt-6">
+            Gerencie suas finanças pessoais de uma forma simples com o fincheck, e o melhor, totalmente de graça!
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default AuthLayout
+export default AuthLayout;
