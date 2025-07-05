4626f7b9bf03517a1adc72676b04698e9f2d48ee
rafaapcode <rafaap2003@gmail.com>
2025-07-05 15:11:57 -0300

    fix: fixing the loading state bug

--- src/view/pages/Dashboard/components/transactions/Transactions.tsx
+++ src/view/pages/Dashboard/components/transactions/Transactions.tsx
@@ -13,12 +13,14 @@ import SliderOption from "./SliderOption";
 import { useTransactionsController } from "./useTransactionsController";
 
 function Transactions() {
-  const { areValuesVisible, isLoading, transactions } =
+  const { areValuesVisible, isLoading, isInitialLoading, transactions } =
     useTransactionsController();
+
+  const hasTransactions = transactions.length > 0;
 
   return (
     <div className="rounded-2xl bg-gray-200 w-full h-full px-4 py-8 md:p-10 flex flex-col">
-      {isLoading && (
+      {isInitialLoading && (
         <div
           className="w-full h-full flex justify-center
        items-center"
@@ -26,7 +28,7 @@ function Transactions() {
           <Spinner className="fill-teal-500 size-10" />
         </div>
       )}
-      {!isLoading && (
+      {!isInitialLoading && (
         <>
           <header className="">
             <div className="flex items-center justify-between">
@@ -61,11 +63,25 @@ function Transactions() {
           </header>
 
           <div className="mt-4 space-y-2 flex-1 overflow-y-auto">
-            {transactions.length === 0 && <div className="flex flex-col justify-center items-center h-full">
-                <img src={emptyStateIllustration} alt="Empty state illustration" />
-                <p className="text-gray-700 mt-4">Não encontramos nenhuma transação</p>
-              </div>}
-            {transactions.length > 0 && (
+            {isLoading && (
+              <div className="flex flex-col justify-center items-center h-full">
+                <Spinner className="fill-teal-500 size-10" />{" "}
+              </div>
+            )}
+
+            {(!hasTransactions && !isLoading) && (
+              <div className="flex flex-col justify-center items-center h-full">
+                <img
+                  src={emptyStateIllustration}
+                  alt="Empty state illustration"
+                />
+                <p className="text-gray-700 mt-4">
+                  Não encontramos nenhuma transação
+                </p>
+              </div>
+            )}
+
+            {hasTransactions && !isLoading && (
               <>
                 <div className="bg-white p-4 rounded-2xl flex items-center justify-between gap-4">
                   <div className="flex-1 flex items-center gap-3">
--- src/view/pages/Dashboard/components/transactions/useTransactionsController.ts
+++ src/view/pages/Dashboard/components/transactions/useTransactionsController.ts
@@ -3,5 +3,5 @@ import useDashboard from "../DashboardContext/useDashboard";
 export function useTransactionsController() {
   const {areValuesVisible} = useDashboard();
 
-  return {areValuesVisible, isLoading: false, transactions: []};
-}
+  return {areValuesVisible,  transactions: [], isInitialLoading: false, isLoading: false};
+} 
