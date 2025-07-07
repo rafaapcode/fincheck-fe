7bedc907f31ddbdee1f683618ad653d596d1a1a6
rafaapcode <rafaap2003@gmail.com>
2025-07-07 20:54:09 -0300

    feat: creating the dropdown menu with radix

---
+++ src/view/components/DropdownMenu.tsx
@@ -0,0 +1,63 @@
+import * as RDXDropdownMenu from "@radix-ui/react-dropdown-menu";
+import type { ReactNode } from "react";
+import { cn } from "../../app/utils/cn";
+
+function DropdownMenuRoot({ children }: { children: ReactNode }) {
+  return <RDXDropdownMenu.Root>{children}</RDXDropdownMenu.Root>;
+}
+
+function DropdownMenuTrigger({ children }: { children: ReactNode }) {
+  return (
+    <RDXDropdownMenu.Trigger className="outline-none">
+      {children}
+    </RDXDropdownMenu.Trigger>
+  );
+}
+
+interface DropdownMenuContent {
+  children: ReactNode;
+  className?: string;
+}
+
+function DropdownMenuContent({ children, className }: DropdownMenuContent) {
+  return (
+    <RDXDropdownMenu.Portal>
+      <RDXDropdownMenu.Content
+        className={cn(
+          "rounded-2xl p-2 bg-white space-y-2 shadow-lg mt-1",
+          className
+        )}
+      >
+        {children}
+      </RDXDropdownMenu.Content>
+    </RDXDropdownMenu.Portal>
+  );
+}
+
+interface DropdownMenuItemProps {
+  children: ReactNode;
+  className?: string;
+  onSelect?: () => void;
+}
+
+function DropdownMenuItem({ children, className,  onSelect }: DropdownMenuItemProps) {
+  return (
+    <RDXDropdownMenu.Item
+      onSelect={onSelect}
+      className={cn(
+        "cursor-pointer outline-none min-h-12 flex items-center py-2 px-4 gap-2 justify-start  text-sm text-gray-800 data-[highlighted]:bg-gray-50 rounded-2xl transition-colors duration-150",
+        className
+      )}
+    >
+      {children}
+    </RDXDropdownMenu.Item>
+  );
+}
+
+const DropdownMenu = {
+  Root: DropdownMenuRoot,
+  Trigger: DropdownMenuTrigger,
+  Content: DropdownMenuContent,
+  Item: DropdownMenuItem,
+};
+export default DropdownMenu;
--- src/view/components/UserMenu.tsx
+++ src/view/components/UserMenu.tsx
@@ -1,10 +1,27 @@
+import { ExitIcon } from "@radix-ui/react-icons";
+import { useAuth } from "../../app/hooks/useAuth";
+import DropdownMenu from "./DropdownMenu";
+
 function UserMenu() {
+  const {signOut} = useAuth();
+  
   return (
-    <div className="bg-teal-100 rounded-full w-12 h-12 flex items-center justify-center border border-teal-200">
-      <span className="text-sm tracking-[-0.5px] font-medium text-teal-800">
-        RA
-      </span>
-    </div>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger>
+        <div className="bg-teal-100 cursor-pointer rounded-full w-12 h-12 flex items-center justify-center border border-teal-200">
+          <span className="text-sm tracking-[-0.5px] font-medium text-teal-800">
+            RA
+          </span>
+        </div>
+      </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content className="w-32">
+          <DropdownMenu.Item className="justify-between" onSelect={signOut}>
+            Sair
+            <ExitIcon className="size-4"/>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 }
 
